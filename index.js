5563c29c00eb8dcbcb4d8073ee8d948ccb80aa31
vibhu mahendru <vibhumahendru@gmail.com>
2018-08-23 15:54:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten (x){
+  var y = kittens.push(x)
+  return y
+  }
+  var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyPrependKitten (x){
+  var y = kittens.unshift(x)
+  return y
+  }
+   var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveLastKitten (x){
+  var y = kittens.pop(x)
+  return y
+  }
+  var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveFirstKitten (x){
+  var y = kittens.shift(x)
+  return y
+  }
+  
+  var kittens = ['Milo', 'Otis', 'Garfield']
+function appendKitten (x){
+  var y = [...kittens, x]
+  return y
+  }
+  
+   var kittens = ['Milo', 'Otis', 'Garfield']
+function prependKitten (x){
+  var y = [x,...kittens]
+  return y
+  }
+  
+  
+  
