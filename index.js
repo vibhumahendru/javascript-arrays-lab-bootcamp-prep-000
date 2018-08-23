de1bf2db9916506e88dc41e0b3d758a8acba4c12
vibhu mahendru <vibhumahendru@gmail.com>
2018-08-23 15:57:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ function prependKitten (x){
   }
   
    var kittens = ['Milo', 'Otis', 'Garfield']
-function removeLastKitten (){
-  var y = kitten.slice(0,1)
+function removeLastKitten (x){
+  var y = kittens.slice(0,1)
   return y
   }
   
